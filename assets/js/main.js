f5c66f1d69a46051dbacf3c1317fa310747348a4
Judith <judith.sanmartin.s@gmail.com>
2024-03-08 02:29:39 -0300

    ciclos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,20 @@
 const numero = +prompt("ingrese un numero entre 1 y 20")
 
-if (numero <= 20 || numero >=1) {
-    alert("el numero es valido esta en el rango");
-} else {
-    alert("Numero fuera del rango");
-}
-
 const factorial = (numero) => {
     if (numero <= 1) return 1
-    return numero * factorial(numero -1)
+    return numero * factorial(numero - 1)
 }
+if (numero <= 20 && numero >= 1) {
+    console.log("el numero es valido esta en el rango");
+    for (let i = 1; i <= numero; i++) {
+        console.log(`${i} X ${numero}= ${i * numero}`)
+    }
+    for (let i = 1; i <= numero; i++) {
+        console.log(`Factorial de ${i} es: ${factorial(i)}`)
+    }
+
+} else {
+    console.log("Numero fuera del rango");
+}
+
+
